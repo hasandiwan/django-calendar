81189be9dc4beec381fe1ffbe9387fa4d82b0c97
Hasan Diwan <hasan@gooutside.com>
2025-01-10 16:49:16 -0800

    urls => path

--- cal/urls.py
+++ cal/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 app_name = 'cal'
 urlpatterns = [
-    url(r'^index/$', views.index, name='index'),
-    url(r'^calendar/$', views.CalendarView.as_view(), name='calendar'),
-    url(r'^event/new/$', views.event, name='event_new'),
-	url(r'^event/edit/(?P<event_id>\d+)/$', views.event, name='event_edit'),
+    path(r'index/', views.index, name='index'),
+    path(r'calendar/', views.CalendarView.as_view(), name='calendar'),
+    path(r'event/new/', views.event, name='event_new'),
+	path(r'event/edit/(?P<event_id>\d+)/', views.event, name='event_edit'),
 ]
